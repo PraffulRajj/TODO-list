5f9ceec30ad76b4d3958321211359b7115205218
Prafful raj <praffulraj123@gmail.com>
2025-07-25 12:18:43 +0530

    created basic CSS for TODO

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ let input = document.getElementById("todoInput");
 let todoList = document.getElementById("todoList");
 
 btn.addEventListener("click", function() {
-    console.log(input.value);
+    let newTodo = document.createElement("li");
+    newTodo.textContent = input.value;
+
+    let removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.addEventListener("click", function() {
+        todoList.removeChild(newTodo);
+    });
+    newTodo.appendChild(removeBtn);
+
+    todoList.appendChild(newTodo);
     input.value = "";
+});
+
+let removeButtons = document.querySelectorAll("button");
+removeButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        let parent = this.parentElement;
+        todoList.removeChild(parent);
+    });
 });
